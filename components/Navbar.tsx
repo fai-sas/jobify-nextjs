4aa7d31083c279932ecaefa84d1029afd99b276f
fai-sas <craftedbyfaisal@gmail.com>
2024-02-03 22:00:21 +0600

    navbar added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,18 @@
+import LinksDropdown from './LinksDropdown'
+import ThemeToggle from './ThemeToggle'
+import { UserButton } from '@clerk/nextjs'
+
 const Navbar = () => {
-  return <h1 className=' text-4xl'>Navbar</h1>
+  return (
+    <nav className='bg-muted py-4 sm:px-16 lg:px-24 px-4 flex items-center justify-between'>
+      <div>
+        <LinksDropdown />
+      </div>
+      <div className='flex items-center gap-x-4'>
+        <ThemeToggle />
+        <UserButton afterSignOutUrl='/' />
+      </div>
+    </nav>
+  )
 }
 export default Navbar
