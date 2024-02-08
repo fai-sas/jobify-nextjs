ecb676f4158626ec2dc0b8fb1eb521f3c1bb02c6
fai-sas <craftedbyfaisal@gmail.com>
2024-02-08 19:38:50 +0600

    providers updated

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,10 @@
 'use client'
 
 import { ThemeProvider } from '@/components/theme-provider'
+import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Toaster } from '@/components/ui/toaster'
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
