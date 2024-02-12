5e3a031bce184e8560bc3726c1e74a29e0e23b0e
fai-sas <craftedbyfaisal@gmail.com>
2024-02-12 20:08:05 +0600

    action type updated

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,37 +1,45 @@
 'use server'
 
-import prisma from './db';
-import { auth } from '@clerk/nextjs';
-import { JobType, CreateAndEditJobType, createAndEditJobSchema } from './types';
-import { redirect } from 'next/navigation';
-import { Prisma } from '@prisma/client';
-import dayjs from 'dayjs';
+import prisma from './db'
+import { auth } from '@clerk/nextjs'
+import { JobType, CreateAndEditJobType, createAndEditJobSchema } from './types'
+import { redirect } from 'next/navigation'
+import { Prisma } from '@prisma/client'
+import dayjs from 'dayjs'
 
 function authenticateAndRedirect(): string {
-  const { userId } = auth();
+  const { userId } = auth()
   if (!userId) {
-    redirect('/');
+    redirect('/')
   }
-  return userId;
+  return userId
 }
-
 
 export async function createJobAction(
   values: CreateAndEditJobType
 ): Promise<JobType | null> {
   // await new Promise((resolve) => setTimeout(resolve, 3000));
-  const userId = authenticateAndRedirect();
+  const userId = authenticateAndRedirect()
   try {
-    createAndEditJobSchema.parse(values);
+    createAndEditJobSchema.parse(values)
     const job: JobType = await prisma.job.create({
       data: {
         ...values,
         clerkId: userId,
       },
-    });
-    return job;
+    })
+    return job
   } catch (error) {
-    console.error(error);
-    return null;
+    console.error(error)
+    return null
   }
 }
+
+type GetAllJobsActionTypes = {
+  search?: string
+  jobStatus?: string
+  page?: number
+  limit?: number
+}
+
+export async function getAllJobsAction() {}
