f96961c0f0dc77b0f2dc786c491fa09eba9814c9
fai-sas <craftedbyfaisal@gmail.com>
2024-02-20 19:41:49 +0600

    button updated

--- components/DeleteJobBtn.tsx
+++ components/DeleteJobBtn.tsx
@@ -1,4 +1,12 @@
-const DeleteJobBtn = () => {
+import { Button } from './ui/button'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { deleteJobAction } from '@/utils/actions'
+import { useToast } from '@/components/ui/use-toast'
+
+const DeleteJobBtn = ({ id }: { id: string }) => {
+  const { toast } = useToast()
+  const queryClient = useQueryClient()
+
   return <h1 className='text-4xl'>DeleteJobBtn</h1>
 }
 export default DeleteJobBtn
