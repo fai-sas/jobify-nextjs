507463d6c28462b16a5a66246ae41501c622b862
fai-sas <craftedbyfaisal@gmail.com>
2024-02-21 18:27:27 +0600

    react query added in delete component

--- components/DeleteJobBtn.tsx
+++ components/DeleteJobBtn.tsx
@@ -6,6 +6,22 @@ import { useToast } from '@/components/ui/use-toast'
 const DeleteJobBtn = ({ id }: { id: string }) => {
   const { toast } = useToast()
   const queryClient = useQueryClient()
+  const { mutate, isPending } = useMutation({
+    mutationFn: (id: string) => deleteJobAction(id),
+    onSuccess: (data) => {
+      if (!data) {
+        toast({
+          description: 'there was an error',
+        })
+        return
+      }
+      queryClient.invalidateQueries({ queryKey: ['jobs'] })
+      queryClient.invalidateQueries({ queryKey: ['stats'] })
+      queryClient.invalidateQueries({ queryKey: ['charts'] })
+
+      toast({ description: 'job removed' })
+    },
+  })
 
   return <h1 className='text-4xl'>DeleteJobBtn</h1>
 }
