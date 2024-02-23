6a573486925970f332be80f23f6f463738762de7
fai-sas <craftedbyfaisal@gmail.com>
2024-02-23 18:24:59 +0600

    single job page created

---
+++ app/(dashboard)/jobs/[id]/page.tsx
@@ -0,0 +1,7 @@
+// import EditJobForm from '@/components/EditJobForm'
+import { getSingleJobAction } from '@/utils/actions'
+
+const SingleJobPage = () => {
+  return <div>SingleJobPage</div>
+}
+export default SingleJobPage
